9391860e6522b02cbb675cd0af3303bf00da33ee
Apoorv Kansal <akansal@zendesk.com>
2018-10-22 22:54:32 +1100

    Add 926

---
+++ problems/926_monotonic_sequence.py
@@ -0,0 +1,32 @@
+'''
+URL: https://leetcode.com/problems/flip-string-to-monotone-increasing/
+Time complexity: O(n)
+Space complexity: O(1)
+'''
+
+class Solution:
+    def minFlipsMonoIncr(self, S):
+        """
+        :type S: str
+        :rtype: int
+        """
+
+        total_zeros = sum(1 for i in range(len(S)-1, -1, -1) if S[i] == '0')
+        total_ones = sum(1 for i in range(len(S)-1, -1, -1) if S[i] == '1')
+        min_flips = total_zeros
+
+        num_zeros = total_zeros  # num zeros in right subarray
+        num_ones = 0 # num ones in left subarray
+
+        divider = 0
+        while divider < len(S):
+            if S[divider] == '0':
+                num_zeros -= 1
+            else:
+                num_ones += 1
+
+            min_flips = min(min_flips, num_zeros + num_ones)
+            divider += 1
+
+        return min(min_flips, total_ones)
+
