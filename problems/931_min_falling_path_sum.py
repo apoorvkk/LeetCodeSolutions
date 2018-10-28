e46f5783b3238c53c19e58287f3d7cea9121dde6
Apoorv Kansal <akansal@zendesk.com>
2018-10-28 15:44:43 +1100

    931

---
+++ problems/931_min_falling_path_sum.py
@@ -0,0 +1,50 @@
+'''
+URL: https://leetcode.com/problems/minimum-falling-path-sum
+Time complexity: O(nm)
+Space complexity: O(nm)
+'''
+
+class Solution:
+    def minFallingPathSum(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: int
+        """
+        memo = [[None for i in range(len(A[0]))] for j in range(len(A))]
+
+        curr_min_sum = float('inf')
+
+        for i in range(len(A)):
+            min_sum = self._min_falling(memo, (0, i), A)
+            curr_min_sum = min(min_sum, curr_min_sum)
+
+        return curr_min_sum
+
+    def _min_falling(self, memo, curr_pos, A):
+        x, y = curr_pos
+
+        next_row = x+1
+
+        if next_row >= len(A):
+            return A[x][y]
+
+        curr_min_sum = float('inf')
+        curr_column_selected = None
+
+        next_columns = [y-1, y, y+1]
+        for next_column in next_columns:
+            if next_column < 0 or next_column >= len(A[0]):
+                continue
+
+            if memo[next_row][next_column] is not None:
+                child_min_sum = memo[next_row][next_column]
+            else:
+                child_min_sum = self._min_falling(memo, (next_row, next_column), A)
+
+            if A[x][y] + child_min_sum < curr_min_sum:
+                curr_min_sum = child_min_sum + A[x][y]
+                curr_column_selected = next_column
+
+        memo[x][y] = curr_min_sum
+        return curr_min_sum
+
